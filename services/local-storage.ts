9a1e1f8b7528eba86e5e518bf02581c73e0da192
Joko Santoso <joko74479@gmail.com>
2024-05-03 00:35:12 +0700

    delete file

--- services/local-storage.ts
+++
@@ -1,8 +0,0 @@
-import { getUserById } from '@/lib/data';
-import { User } from '@prisma/client';
-
-export const local = async (user: User) => {
-    const userCreate = await getUserById(user.id);
-    const id = localStorage.setItem('id', `${userCreate?.id}`);
-    return id;
-};
